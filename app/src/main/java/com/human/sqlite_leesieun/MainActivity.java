33aace0f3f35369fc1554e6f2fa0ecf97cf1bda3
이시은 <tldms0137@naver.com>
2021-03-17 12:49:27 +0900

    20210317 오류 수정

--- app/src/main/java/com/human/sqlite_leesieun/MainActivity.java
+++ app/src/main/java/com/human/sqlite_leesieun/MainActivity.java
@@ -1,18 +1,19 @@
 package com.human.sqlite_leesieun;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.human.sqlite_leesieun.DatabaseTables.StudentTable;
 
@@ -35,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonInsert;
     private Button mButtonUpdate;
     private Button mButtonDelete;
+    //가상키보드제어하는 변수선언
+    private InputMethodManager mInputMethodManager;
 
-    // MainActivity 실행되면, 자동으로 실행되는 메서드 = onCreate()
+    //메인액티비티가 실행되면, 자동으로 실행되는 메서드 onCreate 입니다.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//부모 클래스 초기화(메모리로딩)
@@ -70,144 +73,141 @@ public class MainActivity extends AppCompatActivity {
         btnInsert();
     }
 
-    //EditText 컴포넌트 입력값 없애기
+    //EditText 콤포넌트 입력값 없애기
     private void clearComponent() {
-        //EdiText 객체의 값 비우기
+        //EditText객체의 값 비우기
         mEditTextGrade.setText("");
         mEditTextNumber.setText("");
         mEditTextName.setText("");
+        currentCursorId = -1;//현재 테이블커서ID가 지워졌으니, 초기화시킴
     }
-
-//insert 버튼 클릭 이벤트(아래) ====================================================================================================
+    //insert버튼 클릭이벤트(아래)
     private void btnInsert() {
         mButtonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mEditTextGrade.getText().toString().isEmpty() || mEditTextNumber.getText().toString().isEmpty()){
-                    Toast.makeText(getApplicationContext(),"학년/학번 값은 필수입니다.", Toast.LENGTH_SHORT).show();
+                //"".equals()공백체크 && 오브젝트 == null체크 2개의 조건을 1개로 해결한 매서드 isEmpty() 로 개선.
+                if(mEditTextGrade.getText().toString().isEmpty() || "".equals(mEditTextNumber.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"학년/학번 값은 필수 입니다.",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 final int grade = Integer.parseInt(mEditTextGrade.getText().toString());
                 final int number = Integer.parseInt(mEditTextNumber.getText().toString());
                 final String name = mEditTextName.getText().toString();
-
                 ContentValues contentValues = new ContentValues();
                 contentValues.put(StudentTable.GRADE,grade);
                 contentValues.put(StudentTable.NUMBER,number);
                 contentValues.put(StudentTable.NAME,name);
-
+                //insert쿼리 호출
                 insertData(contentValues);
-                //EditText 컴포넌트 입력값 없애기
+                //EditText 콤포넌트 입력값 없애기
                 clearComponent();
-                //화면 리프레시
+                //화면 리프레스
                 updateList();
+                //키보드 숨기기
+                mInputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
             }
         });
     }
+    //SQLiteDatabase 템플릿 insert메서드 실행
     private void insertData(ContentValues contentValues) {
         mSqLiteDatabase.insert(StudentTable.TABLE_NAME,null,contentValues);
     }
-
-
-//delete 버튼 클릭 이벤트(아래) ====================================================================================================
+    //delete버튼 클릭이벤트(아래)
     private void btnDelete() {
         mButtonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(currentCursorId == -1){
-                    Toast.makeText(getApplicationContext(), "선택된 값이 없습니다.", Toast.LENGTH_SHORT).show();
-                    return;
+                if(currentCursorId == -1) {
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//선택된 값이 없으면 삭제이벤트는 종료하기
                 }
                 //삭제쿼리 호출
                 deleteData(currentCursorId);
-                //EditText컴포넌트 초기화
+                //EditText콤포넌트 값 없애기
                 clearComponent();
-                currentCursorId=-1; //현재 테이블 커서ID가 지워져서 다시 초기화
-
                 //리사이클러뷰 화면 리프레시(아래)
                 updateList();
             }
         });
     }
-
+    //SQLiteDatabase 템플릿 delete메서드 실행
     private void deleteData(int currentCursorId) {
-        //SQLiteDatabase 템플릿 delete메서드 실행
-        mSqLiteDatabase.delete(StudentTable.TABLE_NAME,StudentTable._ID+"="+currentCursorId, null);
+        mSqLiteDatabase.delete(StudentTable.TABLE_NAME,StudentTable._ID+"="+currentCursorId,null);
     }
 
-//update 버튼 클릭 이벤트(아래) ====================================================================================================
+    //update버튼 클릭이벤트(아래)
     private void btnUpdate() {
         mButtonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(currentCursorId == -1){
-                    Toast.makeText(getApplicationContext(), "선택된 값이 없습니다.", Toast.LENGTH_SHORT).show();
-                    return; //클릭이벤트 진행 중지
+                if(currentCursorId == -1) {
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//클릭이벤트 진행중지
                 }
-                //DB 갱신
+                //DB갱신
                 final int grade = Integer.parseInt(mEditTextGrade.getText().toString());
                 final int number = Integer.parseInt(mEditTextNumber.getText().toString());
                 final String name = mEditTextName.getText().toString();
-
-                //쿼리 메서드 호출
+                //쿼리메서드 호출
                 updateData(currentCursorId,grade,number,name);
-
-                //화면 리프레시(아래)
+                //화면 리프레시 재생(아래)
                 updateList();
             }
-       });
+        });
     }
     //update버튼에 대한 쿼리
     private void updateData(int currentCursorId, int grade, int number, String name) {
-        //쿼리 매개변수로 1개의 객체에 값을 담기 위해서 객체 변수 생성(아래)
+        //쿼리 매개변수로 1개의 객체에 값을 담기 위해서 객체변수 생성(아래)
         ContentValues contentValues = new ContentValues();
         contentValues.put(StudentTable.GRADE,grade);
         contentValues.put(StudentTable.NUMBER,number);
         contentValues.put(StudentTable.NAME,name);
-
-        //SQLiteDatabase에 있는 템플릿 메서드 중 update 사용(아래)
+        //SQLiteDatabase에 있는 템플릿메서드중 update사용(아래)
         mSqLiteDatabase.update(StudentTable.TABLE_NAME,contentValues,StudentTable._ID+"="+currentCursorId,null);
     }
 
+    //멤버변수로 객체 초기화(아래)
     private void bindObject() {
         //EditText변수를 객체로 생성(아래3줄)
         mEditTextGrade = findViewById(R.id.editTextGrade);
         mEditTextNumber = findViewById(R.id.editTextNumber);
         mEditTextName = findViewById(R.id.editTextName);
-
         //Button변수를 객체로 생성(아래3줄)
         mButtonInsert = findViewById(R.id.btnInsert);
         mButtonUpdate = findViewById(R.id.btnUpdate);
         mButtonDelete = findViewById(R.id.btnDelete);
+        //키보드제어 객체 생성(아래)
+        mInputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
     }
-
+    //화면 리프레스
     private void updateList() {
         mItemList.clear();
         mItemList.addAll(getAllData());
-        mRecyclerAdapter.notifyDataSetChanged(); //어댑터에 실제 값이 들어가면서 화면에 뿌려짐
+        //안드로이드 전용 클래스이고, 메서드입니다.(아래)
+        mRecyclerAdapter.notifyDataSetChanged();//어댑터에 실제값이 들어가면서 화면에 뿌려짐
     }
-
-    //select 쿼리 결과를 리턴한다.
-    private List getAllData() { 
-        List tableList = new ArrayList(); //studentTable 내용이 담길 예정
-        //쿼리작업
-        String[] projection ={
-                StudentTable._ID, //AutoIncrement 자동증가 PK
+    //셀렉트 쿼리결과를 리턴합니다.
+    private List getAllData() {
+        List tableList = new ArrayList<>();//studentTable내용이 담을 예정.
+        //쿼리작업에 사용될 필드명 바인딩
+        String[] projection = {
+                StudentTable._ID,//AutoIncrement자동증가 PK
                 StudentTable.GRADE,
                 StudentTable.NUMBER,
                 StudentTable.NAME
         };
-        //쿼리 템플릿 메서드 사용(아래), Cursor는 레코드 위치를 가지는 테이블
-        Cursor cursor = mSqLiteDatabase.query(StudentTable.TABLE_NAME, projection,null,null,null, null, "_id desc");
-        //반복문 조건은 커서의 다음 레코드가 존재할 때까지
-        while(cursor.moveToNext()){//StudentTable에 있는 필드값을 하나씩 뽑아서
+        //쿼리 템플릿 메서드사용(아래) Cursor커서는 레코드위치를 가지는 테이블과 같음.
+        Cursor cursor = mSqLiteDatabase.query(StudentTable.TABLE_NAME,projection,null,null,null,null,"_id desc");
+        //반복문조건은 커서테이블의 다음레코드가 있을때까지
+        while (cursor.moveToNext()) {//student테이블에 있는 필드값을 하나씩 뽑아서
             //tableList 리스트객체에 1레코드씩 저장
-            int p_id = cursor.getColumnIndexOrThrow(StudentTable._ID);
+            int p_id = cursor.getInt(cursor.getColumnIndexOrThrow(StudentTable._ID));
             int p_grade = cursor.getInt(cursor.getColumnIndexOrThrow(StudentTable.GRADE));
             int p_number = cursor.getInt(cursor.getColumnIndexOrThrow(StudentTable.NUMBER));
             String p_name = cursor.getString(cursor.getColumnIndexOrThrow(StudentTable.NAME));
-            //매개변수
-            tableList.add(new StudentVO(p_id, p_grade, p_number, p_name));
+            //매개변수가 없는 클래스의 생성자 메서드는 자바가 자동으로 만둘어줌.
+            tableList.add(new StudentVO(p_id,p_grade,p_number,p_name));
         }
         return tableList;
     }
@@ -229,11 +229,10 @@ public class MainActivity extends AppCompatActivity {
                 mEditTextName.setText(studentVO.getmName());
             }
         });
-
-        //리사이클러뷰xml과 어댑터 바인딩
+        //리사이클러뷰xml과 어댑터 바인딩(attach) No adapter attached
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true); //리사이클러 뷰의 높이를 고정한다.
+        recyclerView.setHasFixedSize(true);//리사이클러 뷰의 높이를 고정한다.
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(mRecyclerAdapter); //실제 attach(바인딩)
+        recyclerView.setAdapter(mRecyclerAdapter);//실제 attach(바인딩)
     }
 }
